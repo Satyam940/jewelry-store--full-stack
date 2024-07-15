da3ff0a39e7ae29e3a9073d1d4eca42507b6b9a8
Satyam940 <mkmishra940@gmail.com>
2024-07-15 13:55:06 +0530

    error fixing

--- store/store/wsgi.py
+++ store/store/wsgi.py
@@ -1,7 +1,6 @@
-# wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
 application = get_wsgi_application()
