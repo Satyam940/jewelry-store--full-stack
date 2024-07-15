bdf1fd366c37a2638a6573b98b7a5801cd1d17e4
Satyam940 <mkmishra940@gmail.com>
2024-07-15 17:32:28 +0530

    wsig

--- store/store/wsgi.py
+++ store/store/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.store.settings')
 
 application = get_wsgi_application()
