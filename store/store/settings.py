db8e9d08e6f3f7305bcc1972dbcad300645845a5
Satyam940 <mkmishra940@gmail.com>
2024-07-15 17:47:55 +0530

    settings

--- store/store/settings.py
+++ store/store/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'store.shop',
     'tailwind',
     'theme',
     'django_browser_reload',
