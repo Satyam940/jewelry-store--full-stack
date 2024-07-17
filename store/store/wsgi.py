565df7dcdac32fc908660d634414bb7ed12e009f
Satyam940 <mkmishra940@gmail.com>
2024-07-17 13:27:49 +0530

    wsgi

--- store/store/wsgi.py
+++ store/store/wsgi.py
@@ -1,11 +1,13 @@
 import os
-from django.core.wsgi import get_wsgi_application
 import sys
+from pathlib import Path
 
+# Add the project root directory to the Python path
+root_dir = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(root_dir))
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_root)
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.store.settings')
 
 application = get_wsgi_application()
