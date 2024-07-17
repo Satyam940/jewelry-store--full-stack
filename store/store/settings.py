8663898026e03a44908d2bb2d5cd4ded8c41a9a1
Satyam940 <mkmishra940@gmail.com>
2024-07-17 14:34:15 +0530

    static files

--- store/store/settings.py
+++ store/store/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'store.store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['store.templates'],
+        'DIRS': ['store/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
