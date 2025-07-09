3f0581c41ffcbea19c203ed26c55a0fe7d752c76
Satyam Mishra <143482705+Satyam940@users.noreply.github.com>
2025-07-09 11:24:52 +0530

    Update urls.py

--- store/store/urls.py
+++ store/store/urls.py
@@ -33,4 +33,8 @@ urlpatterns = [
     # path("__reload__/", include("django_browser_reload.urls")),
 
     
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
